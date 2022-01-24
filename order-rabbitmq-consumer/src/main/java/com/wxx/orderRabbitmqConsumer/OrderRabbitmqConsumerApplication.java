f90d2f60dacff2010a742f2d14d47a53929622a3
wxx <1301274502@qq.com>
2022-01-24 15:43:09 +0800

    添加springboot版，遗漏

---
+++ order-rabbitmq-consumer/src/main/java/com/wxx/orderRabbitmqConsumer/OrderRabbitmqConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.wxx.orderRabbitmqConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OrderRabbitmqConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderRabbitmqConsumerApplication.class, args);
+    }
+
+}
---
+++ order-rabbitmq-producer/src/main/java/com/wxx/orderRabbitmqProducer/OrderRabbitmqProducerApplication.java
@@ -0,0 +1,14 @@
+package com.wxx.orderRabbitmqProducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OrderRabbitmqProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderRabbitmqProducerApplication.class, args);
+    }
+
+}
